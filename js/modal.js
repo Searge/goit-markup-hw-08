1f4eb1058f54fa8721042267625f0e88ecdca4bd
Searge <ashikcygan@gmail.com>
2021-06-25 09:47:36 +0300

    :tada:

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+refs.openModalBtn.addEventListener('click', tongleModal);
+refs.closeModalBtn.addEventListener('click', tongleModal);
+
+function tongleModal() {
+  refs.backdrop.classList.toggle('backdrop--is-hidden');
+  refs.modal.classList.toggle('modal--active');
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# GoIT SASS/BEM Project
+
+![Repo size](https://img.shields.io/github/repo-size/Searge/goit-markup-hw-07)
+![Lines of code](https://img.shields.io/tokei/lines/github/searge/goit-markup-hw-07?color=orange)
+![w3c](https://img.shields.io/w3c-validation/default?targetUrl=https%3A%2F%2Fsearge.github.io%2Fgoit-markup-hw-07%2F)
