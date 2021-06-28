e497d59057a6cc6f3f358393a9ad506d3d08c8c1
Searge <ashikcygan@gmail.com>
2021-06-28 17:32:51 +0300

    :iphone: Modal :bug:

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@ refs.openModalBtn.addEventListener('click', tongleModal);
 refs.closeModalBtn.addEventListener('click', tongleModal);
 
 function tongleModal() {
+  document.body.classList.toggle('modal-open');
   refs.backdrop.classList.toggle('backdrop--is-hidden');
   refs.modal.classList.toggle('modal--active');
 }
